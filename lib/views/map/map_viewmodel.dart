46c4a06b07161cc882ca32357d304bdf7b8233dd
me-here <mihirthanekar@gmail.com>
2020-09-09 15:41:53 -0700

    Removed useless code.

--- lib/views/map/map_viewmodel.dart
+++ lib/views/map/map_viewmodel.dart
@@ -13,17 +13,6 @@ class MapViewModel extends ChangeNotifier {
   BitmapDescriptor _followerIcon;
   BitmapDescriptor get followerIcon => _followerIcon;
 
-  // Thanks to Miguel Ruivo for the icon size workaround: https://stackoverflow.com/questions/53633404/how-to-change-the-icon-size-of-google-maps-marker-in-flutter
-  Future<Uint8List> getBytesFromAsset(String path, int width) async {
-    ByteData data = await rootBundle.load(path);
-    Codec codec = await instantiateImageCodec(data.buffer.asUint8List(),
-        targetWidth: width);
-    FrameInfo fi = await codec.getNextFrame();
-    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
-        .buffer
-        .asUint8List();
-  }
-
   Future<BitmapDescriptor> getFollowerIcon() async {
     _followerIcon = await BitmapDescriptor.fromAssetImage(
       ImageConfiguration(devicePixelRatio: 1),
