9509527290e899f53dea7cdb588db6ae6dbdc575
me-here <mihirthanekar@gmail.com>
2020-09-09 15:38:15 -0700

    Added yelling soldier head

--- lib/views/map/map_view.dart
+++ lib/views/map/map_view.dart
@@ -1,20 +1,48 @@
+import 'dart:async';
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:survivalrunner/views/map/map_viewmodel.dart';
 
 class MapView extends StatelessWidget {
+  final Completer<GoogleMapController> _mapController = Completer();
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MapViewModel(),
       builder: (context, child) {
         return FutureBuilder(
-          future: context.watch<MapViewModel>().getInitialCameraPosition(),
+          future: Future.wait(
+            [
+              context.watch<MapViewModel>().getCurrentLocation(),
+              context.watch<MapViewModel>().getFollowerIcon(),
+            ],
+          ),
           builder: (context, snapshot) {
+            print(context.watch<MapViewModel>().location);
+
+            // return false
+            //     ? Image.asset('assets/images/runner_icon.png')
+            //     : Image.asset('assets/images/yelling_soldier_head.png');
             return snapshot.hasData
                 ? GoogleMap(
-                    initialCameraPosition: snapshot.data,
+                    mapType: MapType.normal,
+                    myLocationEnabled: true,
+                    initialCameraPosition: context
+                        .watch<MapViewModel>()
+                        .getInitialCameraPosition(),
+                    markers: {
+                      Marker(
+                        markerId: MarkerId('ffa'),
+                        position: context.watch<MapViewModel>().location,
+                        icon: snapshot.data[1],
+                      )
+                    },
+                    onMapCreated: (controller) {
+                      _mapController.complete(controller);
+                    },
                   )
                 : Center(
                     child: CircularProgressIndicator(
--- lib/views/map/map_viewmodel.dart
+++ lib/views/map/map_viewmodel.dart
@@ -1,18 +1,43 @@
+import 'dart:typed_data';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:survivalrunner/helpers/location_helper.dart';
 
 class MapViewModel extends ChangeNotifier {
   LatLng _location;
+  LatLng get location => _location;
 
-  Future<void> getCurrentLocation() async {
+  BitmapDescriptor _followerIcon;
+  BitmapDescriptor get followerIcon => _followerIcon;
+
+  // Thanks to Miguel Ruivo for the icon size workaround: https://stackoverflow.com/questions/53633404/how-to-change-the-icon-size-of-google-maps-marker-in-flutter
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    Codec codec = await instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
+        .buffer
+        .asUint8List();
+  }
+
+  Future<BitmapDescriptor> getFollowerIcon() async {
+    _followerIcon = await BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: 1),
+      "assets/images/yelling_soldier_head.png",
+    );
+    return _followerIcon;
+  }
+
+  Future<LatLng> getCurrentLocation() async {
     final location = await Location().getLocation();
     _location = LatLng(location.latitude, location.longitude);
   }
 
-  Future<CameraPosition> getInitialCameraPosition() async {
-    await getCurrentLocation();
+  CameraPosition getInitialCameraPosition() {
     return CameraPosition(
       target: _location,
       zoom: 14,
